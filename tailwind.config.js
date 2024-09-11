7d98c82a0e1c83ed275fdcbb0850a87b004dcd3b
JT <jtapiaelenes24@gmail.com>
2024-09-11 13:25:16 +0200

    Comienzo del proyecto, añadidos los estilos del menú en el archivo index.html

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ['./dist/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary:'#0f172a'
+      },
+      fontFamily: {
+        roboto: ['Roboto', ...defaultTheme.fontFamily.sans]
+      },
+    },
+  },
+  plugins: [],
+}
